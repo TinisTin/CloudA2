f1eaf13b7444c915ab23339b8d1df823438a4dc4
Trong Le <105897244+TinisTin@users.noreply.github.com>
2024-10-25 19:11:12 +1030

    Implemented driver class with pickup function. Will now implement the destination function

--- src/nuber/students/Driver.java
+++ src/nuber/students/Driver.java
@@ -2,9 +2,12 @@ package nuber.students;
 
 public class Driver extends Person {
 
+	private Passenger currentPassenger;
 	
 	public Driver(String driverName, int maxSleep)
 	{
+		super(driverName, maxSleep);
+		currentPassenger = null;
 	}
 	
 	/**
@@ -14,9 +17,15 @@ public class Driver extends Person {
 	 * @param newPassenger Passenger to collect
 	 * @throws InterruptedException
 	 */
-	public void pickUpPassenger(Passenger newPassenger)
-	{
-	}
+	 public void pickUpPassenger(Passenger newPassenger) {
+	        currentPassenger = newPassenger; // Store the current passenger
+	        try {
+	            Thread.sleep((int) (Math.random() * maxSleep)); // Sleep for a random time
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt(); // Restore interrupted status
+	        }
+	        System.out.println(name + " picked up " + newPassenger.name); // Print statement for pickup action 
+	    }
 
 	/**
 	 * Sleeps the thread for the amount of time returned by the current 
